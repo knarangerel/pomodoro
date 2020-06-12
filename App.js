d51e24c4a4ef71b28e5b0fd84a392556afc84a13
khaliunn <khaliun.narangerel@tufts.edu>
2020-06-12 17:42:02 -0400

    Finish basic functionality

--- App.js
+++ App.js
@@ -1,12 +1,79 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Vibration } from 'react-native';
+import { vibrate } from './utils';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const FOCUS_TIME = 25 * 60
+const BREAK_TIME = 5 * 60
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      timeLeft: FOCUS_TIME,
+      isBreak: false,
+      isStopped: true,
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.countDown, 1)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  countDown = () => {
+    if (this.state.timeLeft > 0 && !this.state.isStopped) {
+      this.setState(prevState => ({ ...this.state, timeLeft: prevState.timeLeft - 1 }))
+    }
+    if (this.state.timeLeft === 0) {
+      vibrate()
+      if (this.state.isBreak) {
+        this.setState({ timeLeft: FOCUS_TIME, isBreak: false, isStopped: true })
+      } else {
+        this.setState({ timeLeft: BREAK_TIME, isBreak: true, isStopped: true })
+      }
+    }
+  }
+
+  stop = () => {
+    this.setState({ ...this.state, isStopped: true })
+  }
+
+  start = () => {
+    this.setState({ ...this.state, isStopped: false })
+  }
+
+  reset = () => {
+    if (this.state.isBreak) {
+      this.setState({ ...this.state, timeLeft: BREAK_TIME, isStopped: true })
+    } else {
+      this.setState({ ...this.state, timeLeft: FOCUS_TIME, isStopped: true })
+    }
+  }
+
+  formatSingleDigits(n) {
+    return (n < 10) ? ("0" + n) : ("" + n)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titleText}>Pomodoro Timer</Text>
+        <View style={styles.timer}>
+          <Text style={styles.timerText}>{this.formatSingleDigits(parseInt(this.state.timeLeft / 60))}</Text>
+          <Text style={styles.timerText}>:</Text>
+          <Text style={styles.timerText}>{this.formatSingleDigits(this.state.timeLeft % 60)}</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button onPress={this.start} style={styles.button} title="Start" />
+          <Button onPress={this.stop} style={styles.button} title="Stop" />
+          <Button onPress={this.reset} style={styles.button} title="Reset" />
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +81,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+  },
+  titleText: {
+    fontSize: 30,
+  },
+  timer: {
+    flexDirection: 'row',
+  },
+  timerText: {
+    fontSize: 48,
+  },
+  buttonContainer: {
+    display: 'flex',
+  },
+  button: {
+    justifyContent: 'space-around',
   },
 });
